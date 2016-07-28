4cbda19a045846a5f379db1db9422db8dbe0283e
orange-buffalo <orange-buffalo@users.noreply.github.com>
2016-07-28 08:37:30 +0200

    styling invocations list

--- hoopoe-web-view-extension/src/main/resources/static/app/services/helper.js
+++ hoopoe-web-view-extension/src/main/resources/static/app/services/helper.js
@@ -3,7 +3,7 @@ function HelperService() {
   var numberFormats = [];
 
   function _printTime(value) {
-    return moment(new Date(Number(value))).format("DD MMM YY, kk:mm:ss");
+    return moment(new Date(Number(value))).format("kk:mm:ss");
   }
 
   function _printNumber(value, decimals) {
@@ -39,9 +39,48 @@ function HelperService() {
     }
   }
 
+  // credits http://snippetrepo.com/snippets/x-time-ago-timestamp-in-javascript
+  function _timeAgoFromEpochTime(epoch) {
+    var secs = ((new Date()).getTime() / 1000) - epoch/ 1000;
+    Math.floor(secs);
+    var minutes = secs / 60;
+    secs = Math.floor(secs % 60);
+    if (minutes < 1) {
+      return secs + (secs > 1 ? ' seconds ago' : ' second ago');
+    }
+    var hours = minutes / 60;
+    minutes = Math.floor(minutes % 60);
+    if (hours < 1) {
+      return minutes + (minutes > 1 ? ' minutes ago' : ' minute ago');
+    }
+    var days = hours / 24;
+    hours = Math.floor(hours % 24);
+    if (days < 1) {
+      return hours + (hours > 1 ? ' hours ago' : ' hour ago');
+    }
+    var weeks = days / 7;
+    days = Math.floor(days % 7);
+    if (weeks < 1) {
+      return days + (days > 1 ? ' days ago' : ' day ago');
+    }
+    var months = weeks / 4.35;
+    weeks = Math.floor(weeks % 4.35);
+    if (months < 1) {
+      return weeks + (weeks > 1 ? ' weeks ago' : ' week ago');
+    }
+    var years = months / 12;
+    months = Math.floor(months % 12);
+    if (years < 1) {
+      return months + (months > 1 ? ' months ago' : ' month ago');
+    }
+    years = Math.floor(years);
+    return years + (years > 1 ? ' years ago' : ' years ago');
+  }
+
   return {
     printFloat: _printNumber,
     getSmartDuration: _getSmartDuration,
-    printTime: _printTime
+    printTime: _printTime,
+    timeAgoFromEpochTime: _timeAgoFromEpochTime
   };
 }
