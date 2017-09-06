22679f3fd90c26d27063a83f4a8df07b989b7f4e
orange-buffalo <orange-buffalo@users.noreply.github.com>
2017-09-06 07:57:31 +0200

    fixing containers commands and the way we get output from container #23

--- hoopoe-integration-testing/src/itest/java/hoopoe/tests/AgentJarEnvIT.java
+++ hoopoe-integration-testing/src/itest/java/hoopoe/tests/AgentJarEnvIT.java
@@ -1,22 +1,37 @@
 package hoopoe.tests;
 
+import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.testcontainers.containers.Container;
 import org.testcontainers.containers.GenericContainer;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
 
 @Slf4j
 public class AgentJarEnvIT {
 
     private static final GenericContainer container = new GenericContainer("ubuntu:xenial")
-            .withCommand("printenv " + HoopoeIntegrationTest.HOOPOE_AGENT_ENV);
+            .withCommand("tail -f /dev/null");
 
     @ClassRule
     public static HoopoeIntegrationTest integrationTest = new HoopoeIntegrationTest().withHoopoeContainer(container);
 
     @Test
-    public void verifyEnvVariableIsAvailable() throws InterruptedException {
-        TestsCommons.waitForContainerAndAssertOutput(container, HoopoeIntegrationTest.HOOPOE_AGENT_JAR_CONTAINER_PATH);
+    public void verifyEnvVariableIsAvailable() throws IOException, InterruptedException {
+        Container.ExecResult printEnvResult = container.execInContainer(
+                "printenv", HoopoeIntegrationTest.HOOPOE_AGENT_ENV);
+
+        String containerOutput = printEnvResult.getStdout();
+
+        assertThat("We expect container to output something",
+                containerOutput, notNullValue());
+
+        assertThat("We expect container to output some specific thing",
+                containerOutput.trim(), equalTo(HoopoeIntegrationTest.HOOPOE_AGENT_JAR_CONTAINER_PATH));
     }
 
 }
--- hoopoe-integration-testing/src/itest/java/hoopoe/tests/AgentJarFileIT.java
+++ hoopoe-integration-testing/src/itest/java/hoopoe/tests/AgentJarFileIT.java
@@ -1,22 +1,37 @@
 package hoopoe.tests;
 
+import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.testcontainers.containers.Container;
 import org.testcontainers.containers.GenericContainer;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
 
 @Slf4j
 public class AgentJarFileIT {
 
     private static final GenericContainer container = new GenericContainer("ubuntu:xenial")
-            .withCommand("ls " + HoopoeIntegrationTest.HOOPOE_AGENT_JAR_CONTAINER_PATH);
+            .withCommand("tail -f /dev/null");
 
     @ClassRule
     public static HoopoeIntegrationTest integrationTest = new HoopoeIntegrationTest().withHoopoeContainer(container);
 
     @Test
-    public void verifyFileExists() throws InterruptedException {
-        TestsCommons.waitForContainerAndAssertOutput(container, HoopoeIntegrationTest.HOOPOE_AGENT_JAR_CONTAINER_PATH);
+    public void verifyFileExists() throws InterruptedException, IOException {
+        Container.ExecResult printEnvResult = container.execInContainer(
+                "ls", HoopoeIntegrationTest.HOOPOE_AGENT_JAR_CONTAINER_PATH);
+
+        String containerOutput = printEnvResult.getStdout();
+
+        assertThat("We expect container to output something",
+                containerOutput, notNullValue());
+
+        assertThat("We expect container to output some specific thing",
+                containerOutput.trim(), equalTo(HoopoeIntegrationTest.HOOPOE_AGENT_JAR_CONTAINER_PATH));
     }
 
 }
