a5c5256e489f60501c185becf94bad25ffd05f70
orange-buffalo <orange-buffalo@users.noreply.github.com>
2017-10-04 07:36:44 +0200

    rewriting groovy plugins to kotlin; reusing assembly task closes #30

---
+++ buildSrc/src/main/kotlin/hoopoe/gradle/plugin/HoopoeDependencyPlugin.kt
@@ -0,0 +1,45 @@
+package hoopoe.gradle.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.tasks.Copy
+import org.gradle.api.tasks.SourceSetContainer
+import org.gradle.kotlin.dsl.createTask
+import java.util.concurrent.Callable
+
+open class HoopoeDependencyPlugin : Plugin<Project> {
+
+    override fun apply(project: Project) {
+
+        project.plugins.apply("java")
+
+        val sourceSets = project.properties["sourceSets"] as SourceSetContainer
+        val mainSourceSet = sourceSets.findByName("main")!!
+
+        val hoopoeCopyTask = project.createTask("copyHoopoeArtifacts", Copy::class) {
+            from(Callable {
+                project.configurations.getByName("compile").resolvedConfiguration.resolvedArtifacts
+                        .filter { it.classifier == "hoopoe" }
+                        .map { it.file }
+            })
+            into(mainSourceSet.output.resourcesDir)
+        }
+
+        val hoopoeUberTask = project.createTask("hoopoeUberTask", Copy::class) {
+
+            from(Callable {
+                (project.configurations.getByName("runtime").resolvedConfiguration.resolvedArtifacts -
+                        project.configurations.getByName("provided").resolvedConfiguration.resolvedArtifacts)
+                        .filter { it.classifier != "hoopoe" }
+                        .map({ project.zipTree(it.file) })
+            })
+            into(mainSourceSet.java.outputDir)
+            include("**/*.class")
+            includeEmptyDirs = false
+        }
+
+        val processResourcesTask = project.tasks.findByName("processResources")!!
+        processResourcesTask.dependsOn.add(hoopoeCopyTask)
+        processResourcesTask.dependsOn.add(hoopoeUberTask)
+    }
+}
